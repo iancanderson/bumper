5ad9b18fc45306c791b972edb7a50beb91a7741b
Ian C. Anderson <ian@iancanderson.com>
2022-01-03 16:03:17 -0500

    Next up song can't be bumped

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,8 @@ class Song < ApplicationRecord
   def put_in_back_of_queue
     self.queue_position = Song.maximum(:queue_position).to_i + 1
   end
+
+  def at_top_of_queue?
+    queue_position == 1
+  end
 end
